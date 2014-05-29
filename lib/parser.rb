d6c319e601f6d1156986132e80b202d5bf5953d5
Ross Nelson <ross.nelson@gmail.com>
2014-05-29 16:26:46 -0500

    Added the start of a parser

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,10 +1,27 @@
 require 'nokogiri'
 require 'open-uri'
-require 'tmpdir'
 require 'colorize'
 
 class FoodCodeParser
   def self.parse(xml_filename, outdir)
-    # TODO: read in the XML file, spit out a bunch of JSON to outdir
+    xml = Nokogiri::XML.parse(File.read(xml_filename))
+    pages = xml.css('page').to_a
+
+    page_number = 1
+    pages.each {|page|
+      puts "Proccessing page #{page_number}...".green
+      page_text = ''
+
+      page.css('text').each {|text|
+        next unless text.children.count > 0
+        page_text += text.content
+      }
+
+      File.open(outdir + '/' + page_number.to_s + '.json', 'w+') {|f|
+        f.puts(page_text)
+      }
+      page_number += 1
+    }
   end
 end
+
