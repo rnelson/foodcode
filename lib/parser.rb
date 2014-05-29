633b152ecd7104dda0f66175c57724945d389fea
Ross Nelson <ross.nelson@gmail.com>
2014-05-29 09:00:50 -0500

    Initial commit

---
+++ lib/parser.rb
@@ -0,0 +1,10 @@
+require 'nokogiri'
+require 'open-uri'
+require 'tmpdir'
+require 'colorize'
+
+class FoodCodeParser
+  def self.parse(xml_filename, outdir)
+    # TODO: read in the XML file, spit out a bunch of JSON to outdir
+  end
+end
